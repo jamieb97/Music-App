955d6ca6e4e930b0265e1cdecf8439a83a68818b
Jamie-PC\Jamie <JB1650@student.anglia.ac.uk>
2020-04-15 19:06:42 +0100

    sql query

---
+++ DatabaseQuery.sql
@@ -0,0 +1,29 @@
+﻿use master 
+
+go
+
+drop database if exists MusicDB
+
+go
+
+create database MusicDB
+
+go
+
+use MusicDB
+
+go
+
+CREATE TABLE Track (
+    TrackID int not null primary key identity,
+    TrackName varchar (100) null,
+    Artist varchar (100) null,
+    ReleaseDate int null,
+    Explicits bit null,
+    TrackLength time null,
+    TrackLink varchar (255) null
+);
+
+INSERT INTO Track (TrackName, Artist, ReleaseDate, Explicits, TrackLength) VALUES
+('The Morning', 'The Weeknd', 2012, 1, '5:15');
+GO
