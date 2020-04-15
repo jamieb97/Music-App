9bb48261f70639eadd108115c43bab2f1f5241a2
Jamie-PC\Jamie <JB1650@student.anglia.ac.uk>
2020-04-15 19:58:11 +0100

    added models

---
+++ Music_API/Models/MusicDBContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Music_API.Models
+{
+    public partial class MusicDBContext : DbContext
+    {
+        public MusicDBContext()
+        {
+        }
+
+        public MusicDBContext(DbContextOptions<MusicDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Track> Track { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=MusicDB;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Track>(entity =>
+            {
+                entity.Property(e => e.TrackId).HasColumnName("TrackID");
+
+                entity.Property(e => e.Artist)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.TrackLink)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.TrackName)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Music_API/Models/Track.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Music_API.Models
+{
+    public partial class Track
+    {
+        public int TrackId { get; set; }
+        public string TrackName { get; set; }
+        public string Artist { get; set; }
+        public int? ReleaseDate { get; set; }
+        public bool? Explicits { get; set; }
+        public TimeSpan? TrackLength { get; set; }
+        public string TrackLink { get; set; }
+    }
+}
