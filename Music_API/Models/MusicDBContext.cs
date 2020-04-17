93226096b4c0d9d977aaefd7e688102b9ecad679
Jamie-PC\Jamie <JB1650@student.anglia.ac.uk>
2020-04-17 16:32:36 +0100

    created api

--- Music_API/Models/MusicDBContext.cs
+++ Music_API/Models/MusicDBContext.cs
@@ -17,12 +17,14 @@ namespace Music_API.Models
 
         public virtual DbSet<Track> Track { get; set; }
 
+        string connectionString = "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = MusicDB";
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=MusicDB;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer(connectionString);
             }
         }
 
--- Music_API/Startup.cs
+++ Music_API/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Music_API.Models;
 
 namespace Music_API
 {
@@ -25,7 +27,9 @@ namespace Music_API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddControllers();
+            services.AddDbContext<MusicDBContext>(o => o.UseSqlServer(Configuration.GetConnectionString("MusicDb")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
