c9260de1662f8dabe4ffe165abf5c6fa1efc6a1c
Jamie-PC\Jamie <JB1650@student.anglia.ac.uk>
2020-04-24 18:22:47 +0100

    front end

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import NavBar from "./Components/NavBar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component{
+
+  render() {
+  return(
+    <div>
+      <NavBar bg = "dark" variant = "dark"/>
     </div>
-  );
+      
+    );
+  }
+
 }
 
 export default App;
---
+++ my-app/src/Components/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Navbar } from "react-bootstrap/Navbar";
+
+class NavBar extends React.Component {
+    render(){
+        return(
+            <div>
+                <Navbar bg = "dark" variant = "dark">
+                    
+                
+                </Navbar>
+
+
+            </div>
+        )
+    }
+
+}
+
+export default NavBar;
